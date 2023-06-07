ad925038b6dee56178c80767b321eeaf2bd7c271
adrienlau <user.email>
2023-06-07 14:23:41 +0200

    07/06

---
+++ jdfke/src/jdfke/EG23.java
@@ -0,0 +1,105 @@
+package jdfke;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+
+public class EG23 {
+
+	private JFrame frmCe;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					EG23 window = new EG23();
+					window.frmCe.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public EG23() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmCe = new JFrame();
+		frmCe.setTitle("CestDuBrutal");
+		frmCe.setBounds(100, 100, 602, 426);
+		frmCe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmCe.getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("Démarrer une partie");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBackground(new Color(128, 255, 0));
+		btnNewButton.setForeground(new Color(0, 0, 0));
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Affect1.ouvreGestionText(null);
+			}
+		});
+		btnNewButton.setFont(new Font("Calibri", Font.PLAIN, 20));
+		
+		
+		btnNewButton.setBounds(184, 203, 214, 47);
+		frmCe.getContentPane().add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("La bataille des programmes");
+		lblNewLabel.setForeground(new Color(128, 255, 255));
+		lblNewLabel.setFont(new Font("Calibri", Font.PLAIN, 40));
+		lblNewLabel.setBounds(67, 78, 450, 89);
+		frmCe.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setIcon(new ImageIcon(EG23.class.getResource("/imgs/uttflougrand.png")));
+		lblNewLabel_1.setBounds(0, 0, 587, 379);
+		frmCe.getContentPane().add(lblNewLabel_1);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frmCe.setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("fichier");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("ouvrir de fichier");
+		mnNewMenu.add(mntmNewMenuItem);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("enregistrer ");
+		mnNewMenu.add(mntmNewMenuItem_1);
+		
+		JMenu mnNewMenu_1 = new JMenu("configurer");
+		menuBar.add(mnNewMenu_1);
+		
+		JMenu mnNewMenu_2 = new JMenu("aide");
+		menuBar.add(mnNewMenu_2);
+	}
+}
---
+++ jdfke/src/jdfke/Melee.java
@@ -0,0 +1,192 @@
+package jdfke;
+
+import java.awt.EventQueue;
+
+
+import javax.swing.JFrame;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import java.awt.Color;
+public class Melee {
+
+	private JFrame frame;
+
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Melee window = new Melee();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Melee() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 601, 449);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("halle s.");
+		lblNewLabel.setForeground(new Color(255, 0, 0));
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel.setBounds(23, 37, 67, 15);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("quartier");
+		lblNewLabel_1.setForeground(new Color(255, 0, 0));
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel_1.setBounds(20, 187, 93, 15);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel(" bibliothèque");
+		lblNewLabel_2.setForeground(new Color(255, 0, 0));
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel_2.setBounds(307, 37, 119, 15);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("halles i.");
+		lblNewLabel_3.setForeground(new Color(255, 0, 0));
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel_3.setBounds(330, 187, 68, 15);
+		frame.getContentPane().add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("BDE");
+		lblNewLabel_4.setForeground(new Color(255, 0, 0));
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel_4.setBounds(174, 112, 54, 23);
+		frame.getContentPane().add(lblNewLabel_4);
+		
+		JButton btnNewButton = new JButton("moi");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MoiTreve.main(null);
+			}
+		});
+		btnNewButton.setBounds(10, 10, 93, 23);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("advers");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				AdversCas.main(null);
+			}
+		});
+		btnNewButton_1.setBounds(10, 57, 93, 23);
+		frame.getContentPane().add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("moi");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MoiTreve.main(null);
+			}
+		});
+		btnNewButton_2.setBounds(148, 91, 93, 23);
+		frame.getContentPane().add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("advers");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				AdversCas.main(null);
+			}
+		});
+		btnNewButton_3.setBounds(148, 133, 93, 23);
+		frame.getContentPane().add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("moi");
+		btnNewButton_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MoiTreve.main(null);
+			}
+		});
+		btnNewButton_4.setBounds(10, 161, 93, 23);
+		frame.getContentPane().add(btnNewButton_4);
+		
+		JButton btnNewButton_5 = new JButton("advers");
+		btnNewButton_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				AdversCas.main(null);
+			}
+		});
+		btnNewButton_5.setBounds(10, 203, 93, 23);
+		frame.getContentPane().add(btnNewButton_5);
+		
+		JButton btnNewButton_6 = new JButton("moi");
+		btnNewButton_6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MoiTreve.main(null);
+			}
+		});
+		btnNewButton_6.setBounds(320, 10, 93, 23);
+		frame.getContentPane().add(btnNewButton_6);
+		
+		JButton btnNewButton_7 = new JButton("advers");
+		btnNewButton_7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				AdversCas.main(null);
+			}
+		});
+		btnNewButton_7.setBounds(320, 57, 93, 23);
+		frame.getContentPane().add(btnNewButton_7);
+		
+		JButton btnNewButton_8 = new JButton("moi");
+		btnNewButton_8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MoiTreve.main(null);
+			}
+		});
+		btnNewButton_8.setBounds(320, 161, 93, 23);
+		frame.getContentPane().add(btnNewButton_8);
+		
+		JButton btnNewButton_9 = new JButton("advers");
+		btnNewButton_9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				AdversCas.main(null);
+			}
+		});
+		btnNewButton_9.setBounds(320, 203, 93, 23);
+		frame.getContentPane().add(btnNewButton_9);
+		
+		JButton btnNewButton_10 = new JButton("simulate wins");
+		btnNewButton_10.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane jop = new JOptionPane(); 
+				int option = jop.showConfirmDialog(null, "Player1 wins, do you want to end the game?", 
+				"<Titre>", JOptionPane.YES_NO_OPTION, 
+				JOptionPane.QUESTION_MESSAGE);
+				if(option == JOptionPane.OK_OPTION)  System.exit(0); 
+				
+			}
+		});
+		btnNewButton_10.setBounds(148, 232, 119, 23);
+		frame.getContentPane().add(btnNewButton_10);
+		
+		JLabel lblNewLabel_5 = new JLabel("New label");
+		lblNewLabel_5.setIcon(new ImageIcon(Melee.class.getResource("/imgs/uttfloubleugrand.png")));
+		lblNewLabel_5.setBounds(0, 8, 587, 412);
+		frame.getContentPane().add(lblNewLabel_5);
+	}
+}
